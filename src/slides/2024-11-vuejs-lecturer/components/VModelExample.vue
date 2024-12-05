ce8d86d2478f15b533dd81e4e853f490a42552ba
SphereStacking <sphere.stacking@gmail.com>
2024-12-06 01:49:41 +0900

    v-modelのsample componentを追加

---
+++ src/slides/2024-11-vuejs-lecturer/components/VModelExample.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col justify-center items-center border border-gray-300 rounded-md p-4">
+    <input v-model="message" class="border border-gray-300 rounded-md p-1 w-full" />
+    <p>入力されたメッセージ: {{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const message = ref('');
+</script>
+
+<style scoped>
+</style>
